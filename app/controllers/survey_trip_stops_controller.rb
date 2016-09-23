fd5a3d153fe18b20f84461df829718928bf2f6b4
AkivaGreen <green.akiva@outlook.com>
2016-09-23 09:21:51 -0400

    Added instance variables for index and show for both controllers

--- app/controllers/survey_trip_stops_controller.rb
+++ app/controllers/survey_trip_stops_controller.rb
@@ -1,8 +1,10 @@
 class SurveyTripStopsController < ApplicationController
   def index
+    @survey_trip_stops = SurveyTripStop.all
   end
 
   def show
+    @survey_trip_stop = SurveyTripStop.find(params[:id])
   end
 
   def new
--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,8 +1,10 @@
 class SurveysController < ApplicationController
   def index
+    @surveys = Survey.all
   end
 
   def show
+    @survey = Survey.find(params[:id])
   end
 
   def new
