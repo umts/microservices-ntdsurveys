29a06351f20631658223f77e4b742d95fd163f69
AkivaGreen <green.akiva@outlook.com>
2016-10-04 14:25:16 -0400

    basic controller action code for survey trip stops

--- app/controllers/survey_trip_stops_controller.rb
+++ app/controllers/survey_trip_stops_controller.rb
@@ -13,8 +13,12 @@ class SurveyTripStopsController < ApplicationController
   end
 
   def update
+    survey_trip_stop = SurveyTripStop.find(params[:id])
+    survey_trip_stop.update(params[:survey_trip_stop])
   end
 
   def destroy
+    survey_trip_stop = SurveyTripStop.find(params[:id])
+    survey_trip_stop.destroy
   end
 end
