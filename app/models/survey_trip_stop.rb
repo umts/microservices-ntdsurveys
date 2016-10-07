c6633005e96962a9c898c0a9a445d33030b94977
AkivaGreen <green.akiva@outlook.com>
2016-10-07 15:18:26 -0400

    Presence validations for survey trip stops

--- app/models/survey_trip_stop.rb
+++ app/models/survey_trip_stop.rb
@@ -1,3 +1,7 @@
 class SurveyTripStop < ApplicationRecord
   belongs_to :survey
+
+  validates :location, presence: true
+  validates :boarded, presence: true
+  validates :alighted, presence: true
 end
