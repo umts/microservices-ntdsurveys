9d3aa3d8be30b96b7aa0fb80fcc29d9eed43e7c6
AkivaGreen <green.akiva@outlook.com>
2016-10-08 16:00:04 -0400

    Allow nested attributes to be updated

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,5 +1,6 @@
 class Survey < ApplicationRecord
   has_many :survey_trip_stops
+  accepts_nested_attributes_for :survey_trip_stops
 
   validates :date, presence: true
   validates :shift, presence: true
