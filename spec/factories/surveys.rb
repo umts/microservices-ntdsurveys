c9af0ca141f4043d7ee44c12e0f56034ceba2c68
AkivaGreen <green.akiva@outlook.com>
2016-10-14 14:48:46 -0400

    Updated factories and ran full rspec

--- spec/factories/surveys.rb
+++ spec/factories/surveys.rb
@@ -1,8 +1,6 @@
 FactoryGirl.define do
   factory :survey do
-    completed false
-    printed false
-    starting_pax 1
-    in_house false
+    date DateTime.current()
+    sequence(:shift) { |n| "Shift #{n}" }
   end
 end
