a5c985cb771a62412717762e3690d49f3a1b931d
AkivaGreen <green.akiva@outlook.com>
2016-10-19 18:12:25 -0400

    Added post and get route for pdf action

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :surveys do 
     collection do
+      post :PDF
       get :PDF
     end
   end
