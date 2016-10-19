6256f53d2da11e99e3287716beb879ab8998a4ec
AkivaGreen <green.akiva@outlook.com>
2016-10-19 17:29:44 -0400

    Added pdf file for surveys

---
+++ app/pdfs/survey_pdf.rb
@@ -0,0 +1,10 @@
+class SurveyPdf < Prawn::Document
+  def initialize(surveys)
+    super(top_margin: 25, page_size: 'LETTER', page_layout: :landscape)
+    font_size 8
+    survey.each do |survey|
+      
+      start_new_page unless survey == surveys.last
+    end
+  end
+end
