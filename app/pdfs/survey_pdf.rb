998de63438061301ca5be3df36fc4a97f2a84dc0
AkivaGreen <green.akiva@outlook.com>
2016-10-28 16:41:17 -0400

    Code refactored for pdf file - Rubocop fixes

--- app/pdfs/survey_pdf.rb
+++ app/pdfs/survey_pdf.rb
@@ -5,19 +5,12 @@ class SurveyPdf < Prawn::Document
     surveys.each do |survey|
       column_box([0, cursor], columns: 2, width: bounds.width) do
         table([['Survey #', 'Date', 'Shift', 'Time'],
-               [survey.id, survey.date.strftime('%m/%d/%Y'),
-                survey.shift, survey.date.strftime('%H:%M')]])
+               [survey.id, survey.formatted_date, survey.shift,
+                survey.time]])
         move_down 20
         text survey.sentence_location
         move_down 10
-        table_data = [['Time', 'Location', 'Passengers boarded',
-                       'Passengers alighted']]
-        survey.survey_trip_stops.order(:sequence_number).each do |sts|
-          table_data.push([sts.time.strftime('%H:%M'), sts.location,
-                           survey.completed ? sts.boarded : '',
-                           survey.completed ? sts.alighted : ''])
-        end
-        table(table_data, row_colors: %w(ffffff cccccc))
+        table(survey.sts_table, row_colors: %w(ffffff cccccc))
         move_down 10
         text "If this survey runs past your shift, please instruct the \
               next driver to finish the survey.".squish,
