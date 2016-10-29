f467ef3cf8f10cb8d16da065378416559145fab8
AkivaGreen <green.akiva@outlook.com>
2016-10-29 14:37:58 -0400

    Added specs for new sts model methods

--- spec/models/survey_trip_stop_spec.rb
+++ spec/models/survey_trip_stop_spec.rb
@@ -1,3 +1,19 @@
 require 'rails_helper'
 
-describe SurveyTripStop do end
+describe SurveyTripStop do
+  describe 'formatted_time' do
+    it 'returns military time' do
+      time = DateTime.current
+      sts = create :survey_trip_stop, time: time
+      expect(sts.formatted_time).to eql time.strftime('%H:%M')
+    end
+  end
+
+  describe 'date' do
+    it 'returns date of sts' do
+      date = DateTime.current
+      sts = create :survey_trip_stop, time: date
+      expect(sts.date).to eql date.strftime('%m/%d/%Y')
+    end
+  end
+end
