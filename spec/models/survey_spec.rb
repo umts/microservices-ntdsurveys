9819274194076c2c38e985d994433295bb962450
AkivaGreen <green.akiva@outlook.com>
2016-11-04 16:31:48 -0400

    Weeded out DRY code from specs

--- spec/models/survey_spec.rb
+++ spec/models/survey_spec.rb
@@ -1,18 +1,16 @@
 require 'rails_helper'
 
 describe Survey do
+  let(:date) { Time.zone.parse('2010-10-10 7:30') }
+  let(:survey) { create :survey, date: date }
   describe 'formatted_date' do
     it 'returns the formatted date' do
-      date = Time.zone.parse('2010-10-10 7:30')
-      survey = create :survey, date: date
       expect(survey.formatted_date).to eql '10/10/2010'
     end
   end
 
   describe 'time' do
     it 'returns the military time' do
-      time = Time.zone.parse('2010-10-10 7:30')
-      survey = create :survey, date: time
       expect(survey.time).to eql '07:30'
     end
   end
@@ -21,11 +19,11 @@ describe Survey do
     it 'returns the first sts location' do
       survey = create :survey
       create :survey_trip_stop, survey_id: survey.id,
-                                location: 'Haigus Mall', sequence_number: 2
-      sts = create :survey_trip_stop, survey_id: survey.id,
-                                      location: 'Fine Arts Center',
-                                      sequence_number: 1
-      expect(survey.location).to eql sts.location
+                                location: 'Haigis Mall', sequence_number: 2
+      create :survey_trip_stop, survey_id: survey.id,
+                                location: 'Fine Arts Center',
+                                sequence_number: 1
+      expect(survey.location).to eql 'Fine Arts Center'
     end
   end
 end
--- spec/models/survey_trip_stop_spec.rb
+++ spec/models/survey_trip_stop_spec.rb
@@ -1,18 +1,16 @@
 require 'rails_helper'
 
 describe SurveyTripStop do
+  let(:time) { Time.zone.parse('2009-4-20 14:45') }
+  let(:sts) { create :survey_trip_stop, time: time }
   describe 'formatted_time' do
     it 'returns military time' do
-      time = Time.zone.parse('2009-4-20 14:45')
-      sts = create :survey_trip_stop, time: time
       expect(sts.formatted_time).to eql '14:45'
     end
   end
 
   describe 'date' do
     it 'returns date of sts' do
-      date = Time.zone.parse('2009-4-20 14:45')
-      sts = create :survey_trip_stop, time: date
       expect(sts.date).to eql '04/20/2009'
     end
   end
