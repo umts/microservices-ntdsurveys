7c88ca2ab1dc3e0531fe72caa8f418b254b04036
David Faulkenberry <david.faulkenberry@gmail.com>
2017-01-16 16:28:50 +0000

    Alphabetize controller specs by action also

--- spec/controllers/surveys_controller_spec.rb
+++ spec/controllers/surveys_controller_spec.rb
@@ -1,31 +1,6 @@
 require 'rails_helper'
 
 describe SurveysController do
-  describe 'GET #index' do
-    it 'renders the template' do
-      get :index
-      expect(response).to render_template :index
-    end
-  end
-
-  describe 'GET #show' do
-    it 'renders the template' do
-      survey = create :survey
-      get :show, params: { id: survey.id }
-      expect(response).to render_template :show
-      expect(assigns[:survey]).to eq survey
-    end
-  end
-
-  describe 'PUT #update' do
-    it 'updates the survey' do
-      survey = create :survey, starting_pax: 3
-      put :update, params: { id: survey.id, survey: { starting_pax: 1 } }
-      survey.reload
-      expect(survey.starting_pax).to eql 1
-    end
-  end
-
   describe 'DELETE #destroy' do
     let!(:survey) { create :survey }
     let(:submit) { delete :destroy, params: { id: survey.id } }
@@ -44,6 +19,13 @@ describe SurveysController do
     end
   end
 
+  describe 'GET #index' do
+    it 'renders the template' do
+      get :index
+      expect(response).to render_template :index
+    end
+  end
+
   describe 'GET #pdf' do
     it 'will render the pdf' do
       survey = create :survey, printed: false
@@ -51,6 +33,24 @@ describe SurveysController do
       get :pdf, params: { surveys: [survey.id] }
       survey.reload
       expect(survey.printed).to be true
+    end
+  end
+
+  describe 'GET #show' do
+    it 'renders the template' do
+      survey = create :survey
+      get :show, params: { id: survey.id }
+      expect(response).to render_template :show
+      expect(assigns[:survey]).to eq survey
+    end
+  end
+
+  describe 'PUT #update' do
+    it 'updates the survey' do
+      survey = create :survey, starting_pax: 3
+      put :update, params: { id: survey.id, survey: { starting_pax: 1 } }
+      survey.reload
+      expect(survey.starting_pax).to eql 1
     end
   end
 end
