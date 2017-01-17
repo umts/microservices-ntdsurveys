7dfe8770608b61d446c478144f24560eb78f482a
David Faulkenberry <david.faulkenberry@gmail.com>
2017-01-17 15:49:08 +0000

    New microservices engine initializer file

--- config/initializers/microservices_engine.rb
+++ config/initializers/microservices_engine.rb
@@ -1,29 +1,34 @@
 # frozen_string_literal: true
 require 'net/http'
-config_file = 'config/mse_router_info.yml'
+CONFIG_FILE = 'config/mse_router_info.yml'
 
-REPORT_MISSING_CONFIG_KEY = -> (key) {
-  raise ArgumentError, "Missing key #{key} in #{config_file}"
-}
+unless ENV['DISABLE_ROUTER_CHECKIN']
 
-unless File.file?(config_file)
-  raise IOError, 'No router configuration YAML file found'
+  report_missing_config_key = lambda do |key|
+    raise ArgumentError, "Missing key #{key} in #{CONFIG_FILE}"
+  end
+
+  unless File.file?(CONFIG_FILE)
+    raise IOError, 'No router configuration YAML file found'
+  end
+
+  config_data = YAML.load_file(CONFIG_FILE)
+
+  service_name = config_data.fetch 'name', &report_missing_config_key
+  service_url = config_data.fetch 'uri', &report_missing_config_key
+  router_url = config_data.fetch 'router_uri', &report_missing_config_key
+  security_token = config_data.fetch 'security_token', &report_missing_config_key
+
+  router_url = router_url + '/services/register'
+
+  res = Net::HTTP.post_form(
+    URI.parse(router_url),
+    name: service_name,
+    url: service_url,
+    models: config_data['accessible_models'],
+    security_token: security_token
+  )
+
+  raise 'The router API response was invalid' if res.code != '200'
+
 end
-
-config_data = YAML.load_file(config_file)
-
-service_name = config_data.fetch 'name', &REPORT_MISSING_CONFIG_KEY
-service_url = config_data.fetch 'uri', &REPORT_MISSING_CONFIG_KEY
-router_url = config_data.fetch 'router_uri', &REPORT_MISSING_CONFIG_KEY
-security_token = config_data.fetch 'security_token', &REPORT_MISSING_CONFIG_KEY
-router_url = router_url + '/services/register'
-
-res = Net::HTTP.post_form(
-  URI.parse(router_url),
-  name: service_name,
-  url: service_url,
-  models: config_data['accessible_models'],
-  security_token: security_token
-)
-
-raise 'The router API response was invalid' if res.code != '200'
